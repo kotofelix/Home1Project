6f98a7e8fb5f4dc0856c1a53d57ee76a3a262ff8
Dmitrii <kotofelix@yandex.ru>
2022-12-10 17:28:47 +0400

    +метод

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,5 +1,22 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        Test1 test1 = new Test1();
+        test1.tebePrivet();
+        test1.tebePrivet();
+        test1.tebePrivet();
+        test1.tebePrivet();
+
+
+    }
+    //TODO снизу написан какой-то метод!
+    public void tebePrivet() {
+        System.out.println("Вот вам от метода!");
+        System.out.println("Вот вам от метода!!");
+        System.out.println("Вот вам от метода!");
+        System.out.println("Вот вам от метода!!");
+        System.out.println("Вот вам от метода!!");
+        System.out.println("Вот вам от метода!");
+        System.out.println("Вот вам от метода!!");
     }
 }
